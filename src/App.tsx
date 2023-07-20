03db71569d102c9a026981008ff131f3ce4c2eb6
Riley Paul <rileypaul96@gmail.com>
2023-07-20 06:01:51 -0700

    add mouse position function and clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import { QuadTree, Rect } from "./geometry/quadtree";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  let quadtree = useRef<QuadTree | null>(null);
+  const quadtree = useRef<QuadTree | null>(null);
   const [points, setPoints] = useState<Point[]>([]);
+  const [mousePoint, setMousePoint] = useState<Point | undefined>(undefined);
+
+  const getMousePoint = (event: MouseEvent): Point | undefined => {
+    if (!canvasRef.current) return;
+    const rect = canvasRef.current.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    const point = new Point(mouseX, mouseY);
+    return point;
+  };
 
   const draw = (ctx: CanvasRenderingContext2D): void => {
     ctx.fillStyle = "LightCoral";
@@ -13,79 +23,71 @@ function App() {
 
     points.forEach((pt) => pt.draw(ctx));
     quadtree.current?.draw(ctx);
+
+    mousePoint?.draw(ctx, { color: "blue" });
   };
 
-  // draw loop
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas!.getContext("2d") as CanvasRenderingContext2D;
     let animationFrameId: number;
 
+    const handleResize = () => {
+      if (!canvas) return;
+      const width = canvas.offsetWidth || 200;
+      const height = canvas.offsetHeight || 200;
+
+      canvas.style.width = "100%";
+      canvas.style.height = "100%";
+      canvas.width = width;
+      canvas.height = height;
+
+      const boundary = new Rect(width / 2, height / 2, width, height);
+      quadtree.current = new QuadTree(boundary);
+      console.log("adding points"); // only works with this
+      for (let pt of points) quadtree.current.insert(pt);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
     const render = () => {
       context.save();
-      // resizeCanvasToDisplaySize(canvas!, context);
       const { width, height } = context.canvas;
       context.clearRect(0, 0, width, height);
 
       draw(context);
       animationFrameId = window.requestAnimationFrame(render);
-
       context.restore();
     };
     render();
 
     return () => {
+      window.removeEventListener("resize", handleResize);
       window.cancelAnimationFrame(animationFrameId);
     };
   }, [draw]);
 
-  // on window resize
-  useEffect(() => {
-    // Get the canvas element and its parent container
-    const canvas = canvasRef.current;
-    const container = canvas!.parentNode;
-
-    // Set the initial size of the canvas to match its container
-
-    // Function to update canvas size when the window is resized
-    const handleResize = () => {
-      // @ts-ignore
-      const width = container.clientWidth;
-      // @ts-ignore
-      const height = container.clientHeight;
-
-      canvas!.width = width;
-      canvas!.height = height;
-
-      const boundary = new Rect(width / 2, height / 2, width, height);
-      quadtree.current = new QuadTree(boundary);
-    };
-
-    handleResize();
-
-    // Attach the resize event listener
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup: remove the resize event listener when the component is unmounted
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   const placePoints: MouseEventHandler<HTMLCanvasElement> = (event) => {
-    const rect = canvasRef.current!.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-    const point = new Point(mouseX, mouseY);
+    const point = getMousePoint(event.nativeEvent);
+    if (!point) return;
     setPoints((prev) => [...prev, point]);
     quadtree.current?.insert(point);
+  };
+
+  const handleMouseMove: MouseEventHandler<HTMLCanvasElement> = (event) => {
+    const point = getMousePoint(event.nativeEvent);
+    setMousePoint(point);
   };
 
   useEffect(() => console.log(points), [points]);
 
   return (
-    <main className="w-screen h-screen bg-red-500">
-      <canvas ref={canvasRef} onClick={placePoints} />;
+    <main className="w-full h-screen bg-red-500">
+      <canvas
+        ref={canvasRef}
+        onClick={placePoints}
+        onMouseMove={handleMouseMove}
+      />
     </main>
   );
 }
