8b923dd7f585a4e17435c709365928ed77dc0a13
Riley Paul <rileypaul96@gmail.com>
2024-09-11 10:04:59 -0600

    add tooltip to center image button

--- src/app.tsx
+++ src/app.tsx
@@ -17,6 +17,12 @@ import useScrollShadow from "./hooks/use-scroll-shadow";
 import { cn } from "./lib/utils";
 import { Separator } from "./components/ui/separator";
 import useCursorStore from "./lib/stores/cursor-store";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function App() {
   const [image, setImage] = React.useState<HTMLImageElement | undefined>();
@@ -45,82 +51,89 @@ function App() {
   }, [image, canvasProps.ref.current]);
 
   return (
-    <div className="flex h-screen w-full">
-      <aside
-        ref={leftSideRef}
-        className="flex w-60 flex-col gap-2 overflow-y-auto border-r bg-card"
-      >
-        <header
-          className={cn(
-            "sticky top-0 z-50 bg-card p-4 pb-6",
-            isLeftSideScrolled && "shadow",
-          )}
+    <TooltipProvider>
+      <div className="flex h-screen w-full">
+        <aside
+          ref={leftSideRef}
+          className="flex w-60 flex-col gap-2 overflow-y-auto border-r bg-card"
         >
-          <Logo />
-        </header>
-        <section className="flex-1 px-4">
-          <DataTable {...{ coordsConverter, points }} />
-        </section>
-        <footer className="sticky bottom-0 z-50 grid gap-2 border-t bg-card p-4">
-          <Button
-            disabled={points.length === 0}
-            className="w-full"
-            variant="secondary"
-            onClick={clearPoints}
+          <header
+            className={cn(
+              "sticky top-0 z-50 bg-card p-4 pb-6",
+              isLeftSideScrolled && "shadow",
+            )}
           >
-            <i className="fa-solid fa-broom mr-2" />
-            Clear Points
-          </Button>
-          <Download {...{ coordsConverter, points }} />
-        </footer>
-      </aside>
-      <main className="relative flex-1" style={{ cursor }}>
-        {image ? (
-          <>
-            <canvas {...canvasProps} className="h-full w-full" />
+            <Logo />
+          </header>
+          <section className="flex-1 px-4">
+            <DataTable {...{ coordsConverter, points }} />
+          </section>
+          <footer className="sticky bottom-0 z-50 grid gap-2 border-t bg-card p-4">
             <Button
-              className="absolute bottom-4 right-4 rounded-full"
-              size="icon"
-              onClick={() => centerImage(image)}
+              disabled={points.length === 0}
+              className="w-full"
+              variant="secondary"
+              onClick={clearPoints}
             >
-              <i className="fa-solid fa-expand" />
+              <i className="fa-solid fa-broom mr-2" />
+              Clear Points
             </Button>
-          </>
-        ) : (
-          <Dropzone
-            onImageLoad={(img) => {
-              setImage(img);
-              centerImage(img);
-            }}
-          />
-        )}
-        {image && showHelp && <Help />}
-      </main>
-      <aside className="flex w-60 flex-col justify-between overflow-y-auto border-l bg-card">
-        <div>
-          <Bullseye canvasRef={canvasProps.ref} mousePoint={mousePoint} />
-          <Separator />
-          <MouseCoords {...{ coordsConverter, mousePoint }} />
-          <Calibrate {...{ calibrations, setCalibrations }} />
-        </div>
-        <div className="flex w-full justify-between gap-4 p-6">
-          <Toggle
-            id="debug"
-            name="Debug Mode"
-            state={debug}
-            setState={setDebug}
-          />
-          <Button
-            className="rounded-full"
-            size="icon"
-            variant={showHelp ? "default" : "secondary"}
-            onClick={() => setShowHelp(!showHelp)}
-          >
-            ?
-          </Button>
-        </div>
-      </aside>
-    </div>
+            <Download {...{ coordsConverter, points }} />
+          </footer>
+        </aside>
+        <main className="relative flex-1" style={{ cursor }}>
+          {image ? (
+            <>
+              <canvas {...canvasProps} className="h-full w-full" />
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    className="absolute bottom-4 right-4 rounded-full"
+                    size="icon"
+                    onClick={() => centerImage(image)}
+                  >
+                    <i className="fa-solid fa-expand" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent side="left">Center Image</TooltipContent>
+              </Tooltip>
+            </>
+          ) : (
+            <Dropzone
+              onImageLoad={(img) => {
+                setImage(img);
+                centerImage(img);
+              }}
+            />
+          )}
+          {image && showHelp && <Help />}
+        </main>
+        <aside className="flex w-60 flex-col justify-between overflow-y-auto border-l bg-card">
+          <div>
+            <Bullseye canvasRef={canvasProps.ref} mousePoint={mousePoint} />
+            <Separator />
+            <MouseCoords {...{ coordsConverter, mousePoint }} />
+            <Calibrate {...{ calibrations, setCalibrations }} />
+          </div>
+          <div className="flex w-full justify-between gap-4 p-6">
+            <Toggle
+              id="debug"
+              name="Debug Mode"
+              state={debug}
+              setState={setDebug}
+            />
+            <Button
+              className="rounded-full"
+              size="icon"
+              variant={showHelp ? "default" : "secondary"}
+              onClick={() => setShowHelp(!showHelp)}
+            >
+              ?
+            </Button>
+          </div>
+        </aside>
+      </div>
+    </TooltipProvider>
   );
 }
 
--- src/hooks/use-canvas-pan-zoom.ts
+++ src/hooks/use-canvas-pan-zoom.ts
@@ -12,12 +12,25 @@ export default function usePanZoom(
 
   const centerImage = (image: HTMLImageElement) => {
     if (!canvasRef.current) return;
-    setMatrix(() =>
-      new DOMMatrix().translate(
-        (canvasRef.current!.width - image.width) / 2,
-        (canvasRef.current!.height - image.height) / 2,
-      ),
-    );
+    setMatrix(() => {
+      const canvasWidth = canvasRef.current?.width ?? 0;
+      const canvasHeight = canvasRef.current?.height ?? 0;
+
+      const scaleX = canvasWidth / image.width;
+      const scaleY = canvasHeight / image.height;
+      const scale = Math.min(scaleX, scaleY) * 0.8;
+
+      const center = new Point(canvasWidth / 2, canvasHeight / 2);
+
+      return new DOMMatrix()
+        .translateSelf(center.x, center.y)
+        .scaleSelf(scale, scale)
+        .translateSelf(-center.x, -center.y)
+        .translate(
+          (canvasWidth - image.width) / 2,
+          (canvasHeight - image.height) / 2,
+        );
+    });
   };
 
   const mouseDownPanZoom: React.MouseEventHandler<HTMLCanvasElement> = (
