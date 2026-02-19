6d1736eaca54aa5a15be2a9148d83aa1e88b464d
Riley Paul <rileypaul96@gmail.com>
2026-02-19 11:37:26 -0700

    style dropzone

--- src/components/dropzone.tsx
+++ src/components/dropzone.tsx
@@ -41,38 +41,32 @@ const Dropzone: React.FC<Props> = ({ onImageLoad }) => {
   };
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center p-4">
-      <Card.Root className="max-w-lg" >
-        <div className="grid gap-5">
-          <header>
-            <Heading as="h2"  mb="2">
-              Welcome to Digitize Plot
-            </Heading>
-            <Text color="gray" mb="4" asChild>
-              <p>
-                A tool to quickly and painlessly convert images of plotted data
-                into raw points.
-              </p>
+    <div>
+      <Card.Root>
+        <div>
+          <Card.Header>
+            <Heading as="h2">Welcome to Digitize Plot</Heading>
+            <Text color="gray">
+              A tool to quickly and painlessly convert images of plotted data
+              into raw points.
             </Text>
-            <Text color="gray"  mb="1" asChild>
-              <p>
-                To get started, choose an image of a plot to be digitized. Or if
-                you just want to try out the app, start with a sample image.
-              </p>
+            <Text color="gray">
+              To get started, choose an image of a plot to be digitized. Or if
+              you just want to try out the app, start with a sample image.
             </Text>
-          </header>
+          </Card.Header>
           <form action="" className="grid gap-3" onSubmit={onSubmit}>
             <FilePicker selectedFile={file} setSelectedFile={setFile} />
             {error && <Text color="red">{error}</Text>}
-            <div className="grid grid-cols-2 gap-2">
-              <Button variant="subtle" className="w-full" onClick={useSample}>
+            <Card.Footer>
+              <Button variant="subtle" onClick={useSample}>
                 Use Sample Image
               </Button>
-              <Button className="w-full" type="submit" disabled={!file}>
+              <Button type="submit" disabled={!file}>
                 Let's go
                 <i className="fa-solid fa-arrow-right"></i>
               </Button>
-            </div>
+            </Card.Footer>
             <input type="submit" hidden />
           </form>
         </div>
