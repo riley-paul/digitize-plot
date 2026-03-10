bfebe8ed71bcca028a5817041cd8eeab7a838ac5
Riley Paul <rileypaul96@gmail.com>
2026-03-09 22:35:59 -0600

    update tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
   adapter: cloudflare(),
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
